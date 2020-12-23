49f4134328aa8d18783e32a619364692a195b38e
Ryan Chang <rzinshanghai@gmail.com>
2020-12-23 13:10:30 -0500

    reg stat pulled

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask, request, jsonify
 import os
+import time
 from nba_api.stats.static import players
-player_dict = players.get_players()
-print(player_dict)
-#init app
-app = Flask("MYNBA")
+from nba_api.stats.endpoints import playercareerstats
+active_players = players.get_active_players()
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg': 'Welcome to myNba'})
+# f = open('stat.txt', "w")
 
-#Run Server
-if __name__ == "__main__":
-    app.run(debug=True)
+
+for player in active_players:
+    id = player['id']
+   
+    time.sleep(0.5)
+    player_info = playercareerstats.PlayerCareerStats(player_id=id, timeout=1000)
+    player_reg_stat = player_info.career_totals_regular_season.get_dict()
+    player_stat = {"player": player, "reg_season_career_total" : player_reg_stat}
+    print(player_reg_stat)
+    # f.write(str(player_stat) + "\n")
+    
+
+
+
+
+
+# #init app
+# app = Flask("MYNBA")
+
+# @app.route('/', methods=['GET'])
+# def get():
+#     bron = [player for player in player_dict if player['full_name'] == 'LeBron James'][0]
+#     bron_id = bron['id']
+    
+#     return jsonify(bron)
+
+# #Run Server
+# if __name__ == "__main__":
+#     app.run(debug=True) 
---
+++ test.py
@@ -0,0 +1,4 @@
+f = open('stats.txt', "w")
+for i in range(7):
+    f.write('d\n')
+f.close()
