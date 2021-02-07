1c3010ca9b9a119980423e8800444fecac12adeb
Ryan Chang <rzinshanghai@gmail.com>
2021-02-07 23:19:12 +0800

    added regseason stat per season

--- test.py
+++ test.py
@@ -1,4 +1,43 @@
-f = open('stats.txt', "w")
-for i in range(7):
-    f.write('d\n')
-f.close()
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.endpoints import boxscorematchups
+from nba_api.stats.endpoints import leaguegamelog
+from nba_api.stats.endpoints import playercareerstats
+# change api ---
+from nba_api.stats.endpoints import playerprofilev2
+#
+from nba_api.stats.static import players
+import json
+import time
+import os
+import ast
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+import numpy as np
+from flask import Flask, request, jsonify
+
+# active_players = players.get_inactive_players() + players.get_active_players()
+active_players = players.get_active_players()
+
+
+def generateStats():
+    #modify here
+    f = open("PlayerRegSeasonStatBySeason.txt", "a")
+    #---
+    i=0
+    for player in active_players:
+        i +=1
+        id = player['id']
+
+        time.sleep(0.5)
+        #modify here
+        player_info = playerprofilev2.PlayerProfileV2(
+            player_id=id, timeout=1000)
+        player_reg_stat = player_info.season_totals_regular_season.get_dict()
+        # ---
+        player_stat = {"player_info": player,
+                       "stats_per_season": player_reg_stat}
+        # print(player_reg_stat)
+        print(i/len(active_players))
+        f.write(str(player_stat) + "\n")
+
+generateStats()
